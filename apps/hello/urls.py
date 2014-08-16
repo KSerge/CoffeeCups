8bb0606f80d08207098ef8a363e99f66a2a4d494
KSerge <squidvard@gmail.com>
2014-08-16 18:06:55 +0300

    ticket:5 add log out action to urls

--- apps/hello/urls.py
+++ apps/hello/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     url(r'^hello/register/$', register_user, name='register'),
     url(r'hello/login/$', login_user, name='login'),
+    url(r'^hello/logout/$', user_logout, name='logout'),
     url(r'^hello/edit/(?P<person_id>\w+)/$', edit, name='edit'),
     url(r'^hello/requests/$', view_requests, name='requests'),
     url(r'^hello/(?P<person_id>\w+)/$', index, name='view_person'),
