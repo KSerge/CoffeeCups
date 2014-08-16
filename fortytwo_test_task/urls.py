0556c3fee42b677237ea96ad04f7701ff13de572
KSerge <squidvard@gmail.com>
2014-08-16 18:41:29 +0300

    ticket:5 configure upload directory

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^uploads/(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^', include('apps.hello.urls')),
 )
