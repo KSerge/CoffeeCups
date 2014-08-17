050d1bbc8f5d0f025d124ebe3a9f9c493a1303de
KSerge <squidvard@gmail.com>
2014-08-17 13:25:22 +0300

    ticket:9 create command to output info about models

--- apps/hello/management/commands/models_info.py
+++ apps/hello/management/commands/models_info.py
@@ -1,1 +1,17 @@
-__author__ = 'serge'
+from django.core.management.base import BaseCommand
+import apps.hello.models
+import inspect
+
+
+class Command(BaseCommand):
+    help = 'Prints all project models and the count of objects in every model'
+
+    def handle(self, *args, **options):
+        for name, data in inspect.getmembers(apps.hello.models, inspect.isclass):
+            try:
+                objects = data.objects.all()
+            except AttributeError:
+                self.stderr.write('error: could not determine model %s' %name)
+            else:
+                self.stdout.write('Model %s: objects: %d' % (name, objects.count()))
+                self.stderr.write('error: model %s: objects: %d' % (name, objects.count()))
