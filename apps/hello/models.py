66dd4b24be8139d363ce41131598d86d36e17c21
krasovskyserge <squidvard@gmail.com>
2014-08-18 11:31:44 +0300

    ticket:10 add signal tracker

--- apps/hello/models.py
+++ apps/hello/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.db.models.signals import post_save, post_delete
 
 CREATE_ACTION_NAME = 'created'
 EDIT_ACTION_NAME = 'edited'
@@ -26,3 +27,42 @@ class ModelObjectsTracker(models.Model):
     model_name = models.CharField(max_length=50, null=False, blank=False)
     type_of_event = models.CharField(max_length=10, null=False, blank=False)
     created_date = models.DateTimeField(default=timezone.now())
+
+MODEL_NAMES = (Person.__name__, IncomingRequest.__name__,)
+
+
+def get_sender_name(sender):
+    if sender.__name__ in MODEL_NAMES:
+        return sender.__name__
+    else:
+        return ''
+
+
+def model_objects_insert_edit_callback(sender, created, **kwargs):
+    model_name = get_sender_name(sender)
+    if not model_name:
+        return
+
+    tracker = ModelObjectsTracker()
+    tracker.model_name = model_name
+    if created:
+        tracker.type_of_event = CREATE_ACTION_NAME
+    else:
+        tracker.type_of_event = EDIT_ACTION_NAME
+    tracker.save()
+
+
+def model_objects_delete_callback(sender, **kwargs):
+    model_name = get_sender_name(sender)
+    if not model_name:
+        return
+
+    tracker = ModelObjectsTracker()
+    tracker.model_name = model_name
+    tracker.type_of_event = DELETE_ACTION_NAME
+    tracker.save()
+
+post_save.connect(model_objects_insert_edit_callback, sender=Person)
+post_save.connect(model_objects_insert_edit_callback, sender=IncomingRequest)
+post_delete.connect(model_objects_delete_callback, sender=Person)
+post_delete.connect(model_objects_delete_callback, sender=IncomingRequest)
