e457c592a50636852ce7fe97cdc4c0d81e4949ab
krasovskyserge <squidvard@gmail.com>
2014-08-18 19:17:22 +0300

    ticket:13 uncomment tests

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -101,50 +101,50 @@ class HelloAppTestCase(TestCase):
         link = '<a href="{0}">Edit</a>'.format(reverse('edit', kwargs={'person_id': person.id}))
         self.assertTrue(link in response.content)
 
-    # #This test fails on getBarista
-    # def test_model_signals(self):
-    #     tracking_objects = ModelObjectsTracker.objects.filter(
-    #         model_name=Person.__name__,
-    #         type_of_event=CREATE_ACTION_NAME)
-    #     self.assertTrue(tracking_objects.count() == 1)
-    #     person = Person.objects.get(pk=1)
-    #     person.skype = 'Skype Account'
-    #     person.save()
-    #     tracking_objects = ModelObjectsTracker.objects.filter(
-    #         model_name=Person.__name__,
-    #         type_of_event=EDIT_ACTION_NAME)
-    #     self.assertTrue(tracking_objects.count() == 1)
-    #     person.delete()
-    #     tracking_objects = ModelObjectsTracker.objects.filter(
-    #         model_name=Person.__name__,
-    #         type_of_event=DELETE_ACTION_NAME)
-    #     self.assertTrue(tracking_objects.count() == 1)
-    #
-    # #This test fails on getBarista
-    # def test_request_is_stored_to_db(self):
-    #     url = reverse('index')
-    #     response = self.client.get(url)
-    #     url = reverse('requests')
-    #     response = self.client.get(url)
-    #     requests = IncomingRequest.objects.filter(path=reverse('index'))
-    #     self.assertTrue(requests.count() == 1)
-    #     requests = IncomingRequest.objects.filter(path=reverse('requests'))
-    #     self.assertTrue(requests.count() == 1)
-    #
-    # #This test fails on getBarista
-    # def test_request_view(self):
-    #     url = reverse('index')
-    #     response = self.client.get(url)
-    #     url = reverse('requests')
-    #     response = self.client.get(url)
-    #     self.assertTrue(REQUESTS_RESPONSE_KEYWORD in response.context)
-    #     self.assertTrue('<h4>Requests:</h4>' in response.content)
-    #
-    # #This test fails on getBarista
-    # def test_context_processor(self):
-    #     url = reverse('index')
-    #     response = self.client.get(url)
-    #     self.assertTrue(CONTEXT_SETTINGS_KEYWORD in response.context)
-    #     url = reverse('requests')
-    #     response = self.client.get(url)
-    #     self.assertTrue(CONTEXT_SETTINGS_KEYWORD in response.context)
+    #This test fails on getBarista
+    def test_model_signals(self):
+        tracking_objects = ModelObjectsTracker.objects.filter(
+            model_name=Person.__name__,
+            type_of_event=CREATE_ACTION_NAME)
+        self.assertTrue(tracking_objects.count() == 1)
+        person = Person.objects.get(pk=1)
+        person.skype = 'Skype Account'
+        person.save()
+        tracking_objects = ModelObjectsTracker.objects.filter(
+            model_name=Person.__name__,
+            type_of_event=EDIT_ACTION_NAME)
+        self.assertTrue(tracking_objects.count() == 1)
+        person.delete()
+        tracking_objects = ModelObjectsTracker.objects.filter(
+            model_name=Person.__name__,
+            type_of_event=DELETE_ACTION_NAME)
+        self.assertTrue(tracking_objects.count() == 1)
+
+    #This test fails on getBarista
+    def test_request_is_stored_to_db(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        url = reverse('requests')
+        response = self.client.get(url)
+        requests = IncomingRequest.objects.filter(path=reverse('index'))
+        self.assertTrue(requests.count() == 1)
+        requests = IncomingRequest.objects.filter(path=reverse('requests'))
+        self.assertTrue(requests.count() == 1)
+
+    #This test fails on getBarista
+    def test_request_view(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        url = reverse('requests')
+        response = self.client.get(url)
+        self.assertTrue(REQUESTS_RESPONSE_KEYWORD in response.context)
+        self.assertTrue('<h4>Requests:</h4>' in response.content)
+
+    #This test fails on getBarista
+    def test_context_processor(self):
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertTrue(CONTEXT_SETTINGS_KEYWORD in response.context)
+        url = reverse('requests')
+        response = self.client.get(url)
+        self.assertTrue(CONTEXT_SETTINGS_KEYWORD in response.context)
