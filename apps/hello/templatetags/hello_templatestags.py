7e99b424975b419a3f78be56e616cc31c9d3ea44
krasovskyserge <squidvard@gmail.com>
2014-08-28 19:49:52 +0300

    ticket:8 start modifying template tag

--- apps/hello/templatetags/hello_templatestags.py
+++ apps/hello/templatetags/hello_templatestags.py
@@ -1,10 +1,13 @@
 from django import template
 from django.core.urlresolvers import reverse
+from ..models import Person, IncomingRequest
 
+MODEL_NAME_TO_URL_NAME = {Person.__name__: 'person', }
 register = template.Library()
 
-
-def edit_link(person):
+def edit_link(object):
+    # person/1/
+    
     return {
         'link': reverse('edit'),
     }
