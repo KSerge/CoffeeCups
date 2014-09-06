8a14fac4014d5ba8004838e8ff90eade4ff6d85c
KSerge <squidvard@gmail.com>
2014-09-06 16:21:45 +0300

    ticket:9 change command to work with any app model

--- apps/hello/management/commands/models_info.py
+++ apps/hello/management/commands/models_info.py
@@ -1,29 +1,28 @@
 from django.core.management.base import BaseCommand
-# import apps.hello.models
 import inspect
 import os
 from django.conf import settings
+from imp import load_source
 
 
 class Command(BaseCommand):
     help = 'Prints all project models and the count of objects in every model'
 
     def handle(self, *args, **options):
-        models = []
+        module_name_to_path_map = {}
         apps_path = os.path.join(settings.BASE_DIR, 'apps')
         for app_name in os.listdir(apps_path):
             if os.path.isdir(os.path.join(apps_path, app_name)):
                 for module_name in os.listdir(os.path.join(apps_path, app_name)):
                     if os.path.isfile(os.path.join(apps_path, app_name, module_name)):
                         if module_name == 'models.py':
-                            models.append('apps.%s.%s' % (app_name, 'models'))
+                            module_name_to_path_map['apps.%s.%s' % (app_name, 'models')] = \
+                                os.path.join(apps_path, app_name, 'models.py')
 
-        for app_models in models:
+        for module_name in module_name_to_path_map:
             try:
-                self.stderr.write(', '.join(models))
-                import app_models
-                self.stderr.write(', '.join(models))
-                for name, data in inspect.getmembers(app_models, inspect.isclass):
+                module = load_source(module_name, module_name_to_path_map[module_name])
+                for name, data in inspect.getmembers(module, inspect.isclass):
                     try:
                         objects = data.objects.all()
                     except AttributeError:
